6323237021fcc54c47bf7b8d1a50a350afa04156
Abdul Ghaffar <agscontact777@gmail.com>
2025-02-16 09:45:43 +0500

    Login component created

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,5 @@
 "use client"
-import ProtectedRoute from "@/components/ProtectedRoute";
-import Button from "@/components/ui/Button";
-import Loader from "@/components/ui/Loader";
-import AddModeratorIcon from '@mui/icons-material/AddModerator';
+import Login from "@/components/root/Login";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -16,10 +13,7 @@ export default function ProfileCard() {
   const submited = () => { console.log("clicked the button") }
   return (
     <div className="flex items-center justify-center min-h-screen bg-background text-foreground">
-      yes there is something
-      <Button
-        className="!rounded-full !p-3"
-        onClick={clicked} isLoading={isLoading} disabled={isLoading} icon={AddModeratorIcon} />
+      <Login />
 
     </div>
   );
